a0b36d3766d63159b99cd59a097b73cb04a61389
Kacper Grzeszczyk <kacgrzes@gmail.com>
2017-07-28 16:12:49 +0200

    Solution for problem #31

---
+++ Problem31.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Html
+
+
+isPrime : Int -> Bool
+isPrime n =
+    -- your implementation goes here
+    if n < 2 then
+        False
+    else
+        eratos (abs n) (List.range 2 (n // 2))
+
+
+eratos : Int -> List Int -> Bool
+eratos n cs =
+    case cs of
+        [] ->
+            True
+
+        x :: xs ->
+            if n % x == 0 then
+                False
+            else
+                eratos n (List.filter (\y -> (y % x) /= 0) xs)
+
+
+main =
+    Html.text
+        (if test then
+            "Your implementation passed all tests."
+         else
+            "Your implementation failed at least one test."
+        )
+
+
+test : Bool
+test =
+    List.all (\( result, expect ) -> result == expect)
+        [ ( isPrime 36, False )
+        , ( isPrime 10, False )
+        , ( isPrime -1, False )
+        , ( isPrime 1, False )
+        , ( isPrime 0, False )
+        , ( isPrime 120, False )
+        , ( isPrime 2, True )
+        , ( isPrime 23, True )
+        , ( isPrime 6000, False )
+        , ( isPrime 7919, True )
+        , ( isPrime 7911, False )
+        , ( isPrime 63247, True )
+        , ( isPrime 63249, False )
+        ]
