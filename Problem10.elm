1312cdcc46d17f6f7eecaa6b1b0c851410768906
Kacper Grzeszczyk <kacgrzes@gmail.com>
2017-08-02 23:33:46 +0200

    Solution for problem #10

---
+++ Problem10.elm
@@ -0,0 +1,57 @@
+module Main exposing (..)
+
+import Html
+import List exposing (head, length, map)
+import Maybe
+
+
+runLengths : List (List a) -> List ( Int, a )
+runLengths xss =
+    map (\xs -> ( length xs, head xs )) xss |> emptyNothings
+
+
+emptyNothings : List ( Int, Maybe a ) -> List ( Int, a )
+emptyNothings xs =
+    case xs of
+        [] ->
+            []
+
+        ( len, Nothing ) :: rest ->
+            emptyNothings rest
+
+        ( len, Just a ) :: rest ->
+            ( len, a ) :: emptyNothings rest
+
+
+main : Html.Html a
+main =
+    Html.text <|
+        case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ toString x ++ " tests."
+
+
+test : Int
+test =
+    List.length <|
+        List.filter ((==) False)
+            [ runLengths [ [ 1, 1, 1, 1 ], [ 2 ], [ 5, 5 ], [ 2 ], [ 1 ] ]
+                == [ ( 4, 1 ), ( 1, 2 ), ( 2, 5 ), ( 1, 2 ), ( 1, 1 ) ]
+            , runLengths [ [ 2 ], [ 5, 5 ], [ 2 ], [ 1 ] ]
+                == [ ( 1, 2 ), ( 2, 5 ), ( 1, 2 ), ( 1, 1 ) ]
+            , runLengths [ [ 1, 1, 1, 1 ], [ 2 ], [ 5, 5 ] ]
+                == [ ( 4, 1 ), ( 1, 2 ), ( 2, 5 ) ]
+            , runLengths [ [ 1, 1, 1, 1 ] ]
+                == [ ( 4, 1 ) ]
+            , runLengths [ [ "a", "a", "a", "a" ], [ "b" ], [ "c", "c" ], [ "b" ], [ "a" ] ]
+                == [ ( 4, "a" ), ( 1, "b" ), ( 2, "c" ), ( 1, "b" ), ( 1, "a" ) ]
+            , runLengths [ [] ] == []
+            , runLengths [ [], [ "a", "a" ] ] == [ ( 2, "a" ) ]
+            , runLengths [] == []
+            ]
